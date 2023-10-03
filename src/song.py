e2c4f8d99e7acb42cc61987071899eec7dc9af3a
Jan Meineke <jan.meineke@mailbox.org>
2023-10-03 20:30:17 +0200

    download func, progress hook writing setting status

--- src/song.py
+++ src/song.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import yt_dlp
+import os
+from contextlib import chdir
 
 
 class song:
@@ -8,39 +10,103 @@ class song:
             raise Exception('title or link required')
 
         self.infos = {}
-        self.url = ""
-        self.stream_url = ""
         self.title = ""
+        self.url = ""
+        self.channel = ""
         self.duration = ""
-        self.thumbnail = ""
+        self.stream_url = ""
+        self.channel_url = ""
+        self.thumbnail_url = ""
+        self.filename = ""
+        self.downlaod_path = "tmp_media"
         self.valid = False
-        self.ytdl_options = {
+        self.status = "nothing"
+        self.ytdl_options_info = {
             "format": "bestaudio/best",
             "default_search": "auto",
             "noplaylist": True,
-            "title": True
+            "title": True,
+        }
+        self.ytdl_options_download = {
+            "format": "bestaudio/best",
+            'restrictfilenames': True,
+            "noplaylist": True,
+            "progress_hooks": [self.progress_hook],
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'm4a',
+            }]
         }
         self.get_infos(content)
 
     def get_infos(self, content: str):
-        content = self.check_url(content)
+        is_url = True
+        if not self.check_url(content):
+            is_url = False
+        else:
+            content = self.check_url(content)
 
         try:
-            with yt_dlp.YoutubeDL(self.ytdl_options) as ydl:
+            with yt_dlp.YoutubeDL(self.ytdl_options_info) as ydl:
                 infos = ydl.extract_info(content, download=False)
+            self.valid = True
         except Exception as exc:
             raise Exception(f"couldnt find {content}") from exc
 
         self.infos = ydl.sanitize_info(infos)
-        self.url = self.infos["entries"][0]["original_url"]
-        self.stream_url = infos.get('url')
-        self.title = infos.get('title')
-        self.duration = infos.get('duration')
-        self.thumbnail = infos.get('thumbnail')
+        if is_url:
+            self.title = self.infos["fulltitle"]
+            self.url = self.infos["original_url"]
+            self.channel = self.infos['channel']
+            self.duration = self.infos['duration']
+            self.stream_url = self.infos['url']
+            self.channel_url = self.infos['channel_url']
+            self.thumbnail_url = self.infos['thumbnail']
+        else:
+            self.title = self.infos["entries"][0]["fulltitle"]
+            self.url = self.infos["entries"][0]["original_url"]
+            self.channel = self.infos['entries'][0]['channel']
+            self.duration = self.infos['entries'][0]['duration']
+            self.stream_url = self.infos['entries'][0]['url']
+            self.channel_url = self.infos['entries'][0]['channel_url']
+            self.thumbnail_url = self.infos['entries'][0]['thumbnail']
 
     def check_url(self, probe: str):
-        if probe.startswith(("https://", "http://")) and "www.youtube.com/watch?" in probe:
+        if probe.startswith(("https://", "http://")) and ("www.youtube.com/watch?" in probe or "youtu.be/" in probe):
             return probe
         elif probe.startswith("www.youtube.com/watch?"):
             return "https://" + probe
-        return probe
+        return False
+
+    def download(self, directory: str = None):
+        if directory:
+            self.downlaod_path = directory
+
+        if not os.path.exists(self.downlaod_path):
+            try:
+                os.mkdir(self.downlaod_path)
+            except FileExistsError:
+                pass
+            except FileNotFoundError as err:
+                raise Exception(f"cant create dir {self.downlaod_path}") from err
+            except Exception as err:
+                raise Exception(f"cant create dir {self.downlaod_path}") from err
+
+        try:
+            with chdir(self.downlaod_path):
+                try:
+                    with yt_dlp.YoutubeDL(self.ytdl_options_download) as ydl:
+                        result = ydl.extract_info(self.url, download=True)
+                    self.filename = ydl.prepare_filename(result)
+                except Exception as exc:
+                    raise Exception(f"failed downloading {self.url}") from exc
+        except Exception as exc:
+            raise Exception(f"cant go to dir {self.downlaod_path}") from exc
+
+        return self.filename
+
+    def progress_hook(self, d):
+        if d['status'] == "downloading":
+            self.status = f"Downloading {self.title}"
+        elif d['status'] == "finished":
+            self.status = "finished downlaod"
