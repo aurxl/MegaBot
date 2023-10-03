742366ae3839c2996fc2eff03d354be33294e27a
Jan Meineke <jan.meineke@mailbox.org>
2023-10-03 22:24:21 +0200

    song: adding docstrings, w2g: update docstring

--- src/song.py
+++ src/song.py
@@ -1,11 +1,49 @@
 #!/usr/bin/env python
-import yt_dlp
-import os
 from contextlib import chdir
+import os
+import yt_dlp
 
 
 class song:
+    """ song class can hold one song at a time.
+
+    The song or better the audio will be downloaded from YouTube
+    using yt-dlp.
+
+    The song object MUST be initialized with a str containing a video
+    title or valid YoutTube url. If not given a valid url, the given
+    str will be searched on YouTube and an url will be gathered.
+
+    Useful gathered informations are accessible by the class attributes.
+    Those informations will be gathered on object initialization.
+
+    Params:
+    content: str -- str containing wether valid YT url, or buzzwords
+                    that will link to song
+
+    Attributes:
+    infos        -- ytdl extract_info json, a LOT of useful infos
+    title        -- YT title of found video
+    url          -- YT url
+    channel      -- channel name
+    duration     -- video duration
+    stream_url   -- url for googlevideo stream -- COULD be used for
+                    playing in channel. Not working now
+    channel_url  -- YT channel link
+    thumbnail_url -- where to find the thumbnail
+    filename     -- the real file name -- will be gathered ON download
+    valid        -- if the object is holding a valid song that can be played
+    status       -- current status -- useful when downloading the audio
+    ytdl_options_info     -- ytdl options for gathering all the infos
+    ytdl_options_download -- ytdl options for downloading the audio
+    """
+
     def __init__(self, content: str = None):
+        """ initialize object attributes.
+
+        Raise error when wether url, nor title is given.
+        """
+
         if not content:
             raise Exception('title or link required')
 
@@ -39,7 +77,15 @@ class song:
         }
         self.get_infos(content)
 
-    def get_infos(self, content: str):
+    def get_infos(self, content: str) -> None:
+        """ gathering song/ audio file infos
+
+        Always be called on object initialization to set object atributes.
+
+        Params:
+        content: str -- passed from class param
+        """
+
         is_url = True
         if not self.check_url(content):
             is_url = False
@@ -71,14 +117,49 @@ class song:
             self.channel_url = self.infos['entries'][0]['channel_url']
             self.thumbnail_url = self.infos['entries'][0]['thumbnail']
 
-    def check_url(self, probe: str):
+    def reload_infos(self) -> bool:
+        """regather infos
+
+        Return:
+        True if successful
+        """
+
+        try:
+            self.get_infos(self.url)
+            return True
+        except Exception as exc:
+            raise Exception(f"cant reload infos ({self.url})") from exc
+
+    def check_url(self, probe: str) -> str:
+        """ checking if given str is YT link
+
+        Params:
+        probe: str -- str to check
+
+        Return:
+        str     -- if valid
+        False   -- if not valid
+        """
+
         if probe.startswith(("https://", "http://")) and ("www.youtube.com/watch?" in probe or "youtu.be/" in probe):
             return probe
         elif probe.startswith("www.youtube.com/watch?"):
             return "https://" + probe
         return False
 
-    def download(self, directory: str = None):
+    def download(self, directory: str = None) -> str:
+        """ downloading audio file
+
+        When given dir is present, chdir to it and download it there.
+        If not present, it will be created.
+
+        Params:
+        directory: str -- optional download path
+
+        Return:
+        str: -- real filename if download successful
+        """
+
         if directory:
             self.downlaod_path = directory
 
@@ -105,7 +186,15 @@ class song:
 
         return self.filename
 
-    def progress_hook(self, d):
+    def progress_hook(self, d) -> None:
+        """ ytdl download progress
+
+        provided by yt_dlp. Current status will be stored on self.status.
+
+        Params:
+        progress hook object
+        """
+
         if d['status'] == "downloading":
             self.status = f"Downloading {self.title}"
         elif d['status'] == "finished":
--- src/w2g.py
+++ src/w2g.py
@@ -18,8 +18,10 @@ class w2g:
 
     By updating a room, the given video will be played immediately.
 
-    w2g attributes:
-    api_key     -- YOUR w2g API Key
+    Params:
+    api_key -- YOUR w2g API Key
+
+    Attributes:
     stream_key  -- set when creating a room, needed to interact later on
     room_link   -- the room link to share
     bg_color    -- optioanl option, sets background color when creating a room
